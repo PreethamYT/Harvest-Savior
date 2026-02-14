1b308a57b5b2623809b52aeabf75e8633e392352
Suchir Janga <suchirjanga@gmail.com>
2026-02-14 20:33:26 +0530

    added frontend code

---
+++ frontend/crop-disease-app/src/test/java/com/example/crop_disease_app/CropDiseaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crop_disease_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropDiseaseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
